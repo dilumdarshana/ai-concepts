357b8a031e6d8efaa4c70c70a55ba908dd9f1f62
dilumdarshana <dilum.dar@gmail.com>
2026-06-26 07:48:15 +0530

    chore: upgrade Prisma to v7 in langgraph project

--- ts/langgraph/lib/prisma.ts
+++ ts/langgraph/lib/prisma.ts
@@ -1,11 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 // Prevent multiple instances of Prisma Client in development
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter,
   log: ['query', 'error', 'warn'],
 });
 
